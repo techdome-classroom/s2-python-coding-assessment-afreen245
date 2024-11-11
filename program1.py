8230484632924323b94f8e0dbd457328850f09a2
afreen245 <163380050+afreen245@users.noreply.github.com>
2024-11-11 15:39:49 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,23 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        stack = []
+        matching_brackets = { ')' : '(','[' : '[','}':'{'}
+        for char in s:
+            if char in matching_brackets:
+                top_element = stack.pop() if stack else '#'
+                if matching_brackets[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+solution = Solution()
+print(solution.isValid("()"))
+print(solution.isValid("()[]{}"))
+print(solution.isValid("(]"))
+print(solution.isValid("([)]"))
+print(solution.isValid("{[]}"))
+
+
 
 
 
