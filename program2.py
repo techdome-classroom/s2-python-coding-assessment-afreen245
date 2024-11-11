8af403787bf9d3c3d7649b4e4497fa499540a406
afreen245 <163380050+afreen245@users.noreply.github.com>
2024-11-11 15:51:22 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,26 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman_to_int = {
+            'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50,
+            'C' : 100, 'D' : 500, 'M' : 1000
+        }
+        total = 0
+        prev_value = 0
+        for char in reversed(s):
+            current_value = roman_to_int[char]
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                 total += current_value
+                prev_value = current_value
+            return total
+solution = Solution()
+print(solution.romanToInt("III"))
+print(solution.romanToInt("LVIII"))
+print(solution.romanToInt("MCMXCIV"))
+print(solution.romanToInt("IX"))
+print(solution.romanToInt("XL"))
+print(solution.romanToInt("CDXLIV"))
 
 
 
